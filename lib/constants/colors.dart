59da193cf4dbe4fa677a3cb655595a136ad3b671
Your Name <byamugishanthony@gmail.com>
2025-07-14 18:58:14 +0300

    added login buttons to the welcome page for people already having accconts

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// Custom color constants
+const Color maroon = Color(0xFF800000); // Standard maroon
+const Color lightCyan = Color(0xFFE0FFFF); // Light cyan 
