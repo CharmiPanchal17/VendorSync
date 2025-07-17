b5d1b1fb93621778637f951622a925ea6a793715
juliakiconco <juliakiconco@gmail.com>
2025-07-17 17:46:45 +0300

    mockdata

---
+++ lib/mock_data/mock_order_data.dart
@@ -0,0 +1,102 @@
+final List<Map<String, dynamic>> categoriesData = [
+  {
+    'name': 'Bakery Products',
+    'items': ['Bread', 'Cakes', 'Mandazi', 'Doughnuts'],
+    'suppliers': ['Golden Crust Ltd', 'Bakery Express', 'Sweet Treats']
+  },
+  {
+    'name': 'Beverages',
+    'items': ['Soda', 'Juice', 'Water', 'Energy Drinks'],
+    'suppliers': ['RefreshCo', 'Drink Masters', 'AquaPure']
+  },
+  {
+    'name': 'Cereals & Grains',
+    'items': ['Maize Flour', 'Millet', 'Rice', 'Oats'],
+    'suppliers': ['GrainHub', 'Cereal World', 'Harvesters']
+  },
+  {
+    'name': 'Cleaning Supplies',
+    'items': ['Soap', 'Detergents', 'Bleach', 'Disinfectants'],
+    'suppliers': ['CleanPro', 'Sparkle Supplies', 'Hygiene Plus']
+  },
+  {
+    'name': 'Condiments & Sauces',
+    'items': ['Royco', 'Tomato Sauce', 'Soy Sauce', 'Peanut Butter'],
+    'suppliers': ['Saucy Ltd', 'Flavor House', 'Condiment King']
+  },
+  {
+    'name': 'Cosmetics & Beauty Products',
+    'items': ['Lotions', 'Makeup', 'Petroleum Jelly'],
+    'suppliers': ['BeautyMart', 'Glow Cosmetics', 'Pretty Supplies']
+  },
+  {
+    'name': 'Dairy Products',
+    'items': ['Milk', 'Yogurt', 'Cheese', 'Ghee'],
+    'suppliers': ['DairyBest', 'Creamy Delights', 'Milkman Ltd']
+  },
+  {
+    'name': 'Deli & Ready-to-Eat Foods',
+    'items': ['Samosas', 'Chapati', 'Cooked Meat'],
+    'suppliers': ['QuickBite', 'Deli Fresh', 'ReadyEats']
+  },
+  {
+    'name': 'Electronics & Accessories',
+    'items': ['Batteries', 'Chargers', 'Earphones'],
+    'suppliers': ['Techie Supplies', 'GadgetMart', 'ElectroHub']
+  },
+  {
+    'name': 'Frozen Foods',
+    'items': ['Frozen Chicken', 'Chips', 'Sausages'],
+    'suppliers': ['Frosty Foods', 'Chill & Grill', 'Frozen Delights']
+  },
+  {
+    'name': 'Fruits & Vegetables',
+    'items': ['Matooke', 'Tomatoes', 'Onions', 'Cabbage'],
+    'suppliers': ['FreshFarm', 'VeggieMart', 'Green Basket']
+  },
+  {
+    'name': 'Grains & Legumes',
+    'items': ['Beans', 'Peas', 'Groundnuts'],
+    'suppliers': ['LegumeLand', 'NutriBeans', 'PulsePro']
+  },
+  {
+    'name': 'Health & Pharmacy Items',
+    'items': ['Painkillers', 'Sanitary Pads', 'Cough Syrup'],
+    'suppliers': ['MediPlus', 'HealthMart', 'PharmaDirect']
+  },
+  {
+    'name': 'Homeware & Utensils',
+    'items': ['Plates', 'Cups', 'Plastic Basins'],
+    'suppliers': ['HomeEssentials', 'Utensil World', 'Household Hub']
+  },
+  {
+    'name': 'Infant & Baby Products',
+    'items': ['Diapers', 'Baby Wipes', 'Baby Food'],
+    'suppliers': ['BabyCare', 'InfantMart', 'TinyTots']
+  },
+  {
+    'name': 'Kitchen Staples',
+    'items': ['Cooking Oil', 'Salt', 'Sugar', 'Tea Leaves'],
+    'suppliers': ['StapleMart', 'KitchenPro', 'Pantry Supplies']
+  },
+  {
+    'name': 'Meat & Poultry',
+    'items': ['Fresh Beef', 'Goat Meat', 'Chicken'],
+    'suppliers': ['MeatMasters', 'Butcher Bros', 'Poultry Place']
+  },
+  {
+    'name': 'Personal Care',
+    'items': ['Toothpaste', 'Bathing Soap', 'Deodorants'],
+    'suppliers': ['CarePlus', 'PersonalPro', 'HygieneMart']
+  },
+  {
+    'name': 'Snacks & Confectionery',
+    'items': ['Biscuits', 'Crisps', 'Sweets', 'Chocolate'],
+    'suppliers': ['SnackTime', 'Sweet Treats', 'Confectionery Co']
+  },
+  {
+    'name': 'Stationery & School Supplies',
+    'items': ['Books', 'Pens', 'Glue', 'Rulers'],
+    'suppliers': ['EduSupplies', 'Stationery Hub', 'SchoolMart']
+  },
+]; 
