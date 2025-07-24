e3627fb81c1d8c64d04d4d4e122bcb57ac2a023b
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-07-24 22:25:46 +0300

    order details management

--- lib/screens/supplier/order_details_screen.dart
+++ lib/screens/supplier/order_details_screen.dart
@@ -340,7 +340,7 @@ class _SupplierOrderDetailsScreenState extends State<SupplierOrderDetailsScreen>
                                 : () => _showUpdateConfirmation(order.id),
                             child: _isLoading
                                 ? const SizedBox(
-                                    width: 20,
+                                    width: 30,
                                     height: 20,
                                     child: CircularProgressIndicator(
                                       strokeWidth: 2,
