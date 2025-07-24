7eef1ba50b744a99a28624eecb3605c41c073a36
Your Name <byamugishanthony@gmail.com>
2025-07-24 12:25:20 +0300

    Changed ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.vendorsync"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21 // Set to 21 for NDK and plugin compatibility
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
