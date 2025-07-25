ec196c0f4f9c7214aa9ccbe16cc517d9b1ce1b4f
priscilla199 <kobumanzipriscilla@gmail.com>
2025-07-25 21:52:20 +0300

    second  commit

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const csv = require('csv-parser');
+const { Storage } = require('@google-cloud/storage');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+Future<void> uploadSalesReport(String vendorId, File file) async {
+  final storageRef = FirebaseStorage.instance.ref()
+      .child('sales_reports/$vendorId/${DateTime.now().toIso8601String()}.csv');
+  await storageRef.putFile(file);
+}
